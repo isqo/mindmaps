9d8ba75762543a7a0e2612cfa1e4a76a86394e21
Ismael qouiqa <qouiqaismael890@address.com>
2025-02-08 19:21:35 +0100

    test

--- app.py
+++ app.py
@@ -297,4 +297,4 @@ def clone_a_node():
 
 if __name__ == "__main__":
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
-    app.run(ssl_context="adhoc")
+    app.run(host="0.0.0.0", port=8000, debug=True)
